3014e5f7d5cdec8ce852a303ce27be99477e6484
mohit sharma <mohit@chalkstreet.com>
2020-04-29 02:07:51 +0530

    leetcode zig-zag-conversion accepted solution

--- leetcode/zig-zag-conversion.py
+++ leetcode/zig-zag-conversion.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-str = 'mohitsharma'
+str = 'PAYPALISHIRING'
 steps = len(str)
-max_row_count = 4
+max_row_count = 3
 no_columns = len(str)
 no_rows = max_row_count - 1
 matrix = []
@@ -16,7 +16,7 @@ init_col_count = 0
 count = 0
 while(count < steps):
     if(init_row_count == 0):
-        while(init_row_count < max_row_count):
+        while(init_row_count < max_row_count and count < steps):
             matrix[init_row_count][init_col_count] = str[count]
             count+=1
             init_row_count += 1
@@ -31,11 +31,11 @@ while(count < steps):
         matrix[init_row_count][init_col_count] = str[count]
         count+=1
         print('small up', init_row_count, init_col_count)
-
-''' 
-print matrix
-'''
+ansString = ''
 for row in range(0, max_row_count):
     for col in range(0, no_columns):
-        print(matrix[row][col],end="")
-    print("")
+        if(matrix[row][col] != 0):
+            ansString += matrix[row][col]
+        # print(matrix[row][col],end="")
+    # print("")
+print(ansString)
